0beb85295d037e3d5cf5af5d9f2fbde784fefe67
do-hyun123 <shickcoong777@gmail.com>
2025-07-15 13:46:33 +0900

    set :: design-token 세팅

--- packages/app-user/App.tsx
+++ packages/app-user/App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaProvider>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        style="light" />
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
 
