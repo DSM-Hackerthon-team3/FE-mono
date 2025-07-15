3937b81637d8776e2a82b09a243d7851e3892fb3
Kanghee <kh63167574@gmail.com>
2025-07-15 16:37:02 +0900

    set :: design token user 적용

--- packages/web-user/src/index.tsx
+++ packages/web-user/src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { StyledProvider } from './style/StyledProvider'
 
 const root = createRoot(
   document.getElementById("root") as HTMLElement
@@ -8,6 +9,8 @@ const root = createRoot(
 
 root.render(
   <StrictMode>
-    <App />
+    <StyledProvider>
+      <App />
+    </StyledProvider>
   </StrictMode>,
 )
