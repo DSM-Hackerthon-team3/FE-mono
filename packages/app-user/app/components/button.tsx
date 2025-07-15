ab7aea900293f75c83a9947a0b188b0cbe0428ba
HYUNA <phyunaa0525@gmail.com>
2025-07-15 16:20:04 +0900

    feat :: button components

---
+++ packages/app-user/app/components/button.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { color } from "../../../design-token/src/theme";
+
+interface IButtonProps {
+  children?: React.ReactNode;
+  onPress: () => void;
+  disabled: boolean;
+}
+
+export const Button = ({ children, onPress, disabled }: IButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.buttoncontainer,
+        { backgroundColor: disabled ? color.main[100] : color.main[400] },
+      ]}
+      onPress={onPress}
+      disabled={disabled}
+    >
+      <Text style={styles.text}>{children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttoncontainer: {
+    backgroundColor: color.main[400],
+    padding: 16,
+    borderRadius: 10,
+  },
+  text: {
+    color: color.white,
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
